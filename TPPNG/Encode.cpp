da90f8ae3aefabd5ff526963a689a78fc8727b1d
Defalt <Defalt@DEFALT-PC>
2018-10-07 17:47:18 -0300

    ass

--- TPPNG/Encode.cpp
+++ TPPNG/Encode.cpp
@@ -4,10 +4,9 @@
 #include "Encode.h"
 #include "lodepng.h"
 
-bool Encode(Image& image, unsigned int threshold)
+bool Encode(Image& image, unsigned int threshold)		
 {
 	unsigned  w = image.width, h = image.height;		//recupero datos de la imagen
-	char buffer[8];										//necesito un bufffer para el itoa
 	ofstream output;													
 	string Infile(image.getFilename().c_str());
 	for (int i = 3; i != 0; i--)
@@ -16,18 +15,18 @@ bool Encode(Image& image, unsigned int threshold)
 	}
 	string outfile = Infile + "BOI";					//pongo terminacion nueva
 	output.open(outfile);								//creo el archivo
+	char buffer[8];										//necesito un bufffer para el itoa
 	char * size = _itoa(w*h, buffer, 10);
 	output << size;														
 	EncodeRec(image.img, output, threshold, w, w);
-
+	return true;
 }
 
-void EncodeRec(unsigned char * img, ofstream& output, unsigned int threshold, unsigned length, unsigned BMPsize)
+void EncodeRec(unsigned char * img, ofstream& output, int threshold, unsigned length, unsigned BMPsize)
 {
 	unsigned char Rprom, Gprom, Bprom;
-	int puntos = puntaje(img, length, BMPsize);
-	
-	if (puntos > threshold)
+		
+	if ((puntaje(img, length, BMPsize)) > threshold)
 	{
 		output << 'N';
 		int corrimiento1 = (length / 2) * 4;					
@@ -42,10 +41,10 @@ void EncodeRec(unsigned char * img, ofstream& output, unsigned int threshold, un
 	else
 	{
 		output << 'H';
-		Rprom = promedioR(unsigned char * img, unsigned length, unsigned BMPsize);
-		Gprom = promedioG(unsigned char * img, unsigned length, unsigned BMPsize);
-		Bprom = promedioB(unsigned char * img, unsigned length, unsigned BMPsize);
-
+		Rprom = promedioR(img, length,  BMPsize);
+		Gprom = promedioG(img, length,  BMPsize);
+		Bprom = promedioB(img, length,  BMPsize);
+		output << Rprom << Gprom << Bprom << 255;
 		
 	}
 }
@@ -53,87 +52,75 @@ void EncodeRec(unsigned char * img, ofstream& output, unsigned int threshold, un
 unsigned char promedioR(unsigned char * img, unsigned length, unsigned BMPsize)
 {
 	unsigned char Rvalue = 0;
-	for (unsigned int fila = 0; fila < length; fila++)
+	for (unsigned int fila = 0; fila < length; fila++)		//recorro todo el quadtree calculando el promedio
 	{
 		for (unsigned int columna = 0; columna < length; columna++)
 		{
-			Rvalue += img[4 * (fila*BMPsize + columna)]
+			
+			Rvalue += img[4 * (fila*BMPsize + columna)];
 		}
 
 	}
 
-	return ((unsigned char)Rvalue / pow(2,BMPsize))
+	return ((unsigned char)Rvalue / pow(2, BMPsize));
 
 }
 
 unsigned char promedioG(unsigned char * img, unsigned length, unsigned BMPsize)
 {
-	unsigned char Gvalue = 0;
-	for (unsigned int fila = 0; fila < length; fila++)
+	unsigned char Gvalue = 0;			
+	for (unsigned int fila = 0; fila < length; fila++)					//recorro todo el quadtree calculando el promedio
 	{
 		for (unsigned int columna = 0; columna < length; columna++)
 		{
-			Gvalue += img[4 * (fila*BMPsize + columna)+1]
+			Gvalue += img[4 * (fila*BMPsize + columna) + 1];
 		}
 
 	}
 
-	return ((unsigned char)Gvalue / pow(2, BMPsize))
+	return ((unsigned char)Gvalue / pow(2, BMPsize));
 
 }
 
 unsigned char promedioB(unsigned char * img, unsigned length, unsigned BMPsize)
 {
 	unsigned char Bvalue = 0;
-	for (unsigned int fila = 0; fila < length; fila++)
+	for (unsigned int fila = 0; fila < length; fila++)						//recorro todo el quadtree calculando el promedio
 	{
 		for (unsigned int columna = 0; columna < length; columna++)
 		{
-			Bvalue += img[4 * (fila*BMPsize + columna) + 2]
+			Bvalue += img[4 * (fila*BMPsize + columna) + 2];
 		}
 
 	}
 
-	return ((unsigned char)Bvalue / pow(2, BMPsize))
+	return ((unsigned char)Bvalue / pow(2, BMPsize));
 
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 int puntaje(unsigned char * image, unsigned length, unsigned BMPsize)		//esto establece un puntaje en genral comparando los minimos y maximos
 {
-	unsigned char Rmax, Gmax, Bmax, Rmin, Gmin, Bmin;
-	Rmax = Gmax = Bmax = 255;
+	unsigned char Rmax, Gmax, Bmax, Rmin, Gmin, Bmin;				
+	Rmax = Gmax = Bmax = 0xFF;
 	Rmin = Gmin = Bmin = 0;
 
-	for (int fila = 0; fila < length; fila++)
+	for (unsigned int fila = 0; fila < length; fila++)					//recorro todo el raw pixel data
 	{
-		for (int columna = 0; columna < length; columna++)		//paso a buscar los valores minimos y maximos de los colores
+		for (unsigned int columna = 0; columna < length; columna++)		//paso a buscar los valores minimos y maximos de los colores
 		{
 			
-			if (image[4 * (fila * BMPsize + columna) + 1] < Gmin)		//analizo los minimos
+			if (image[4 * (fila * BMPsize + columna) + 1] < Gmin)		//busco el minimo verde
 			{
-				Gmin = image[4 * (fila * BMPsize + columna) + 1];		//analizo el verde
+				Gmin = image[4 * (fila * BMPsize + columna) + 1];		
 			}
 			if (image[4 * (BMPsize * fila + columna)] < Rmin)
 			{
-				Rmin = image[4 * (fila * BMPsize + columna)];			//analizo el rojo
+				Rmin = image[4 * (fila * BMPsize + columna)];			////busco el minimo rojo
 			}
 			if (image[4 * (fila * BMPsize + columna) + 2] < Bmin)
 			{
-				Bmin = image[4 * (fila * BMPsize + columna) + 2];		//analizo el azul
+				Bmin = image[4 * (fila * BMPsize + columna) + 2];		////busco el minimo azul
 			}
 						
 			if (image[4 * (fila * BMPsize + columna) + 1] > Gmax)		//idem pero con maximos
--- TPPNG/Encode.h
+++ TPPNG/Encode.h
@@ -4,7 +4,11 @@
 using namespace std;
 
 bool Encode(Image& image, unsigned int treshold);
+void EncodeRec(unsigned char * img, ofstream& output, int threshold, unsigned length, unsigned BMPsize);
 int puntaje(unsigned char * image, unsigned length, unsigned BMPsize);
+unsigned char promedioR(unsigned char * img, unsigned length, unsigned BMPsize);
+unsigned char promedioG(unsigned char * img, unsigned length, unsigned BMPsize);
+unsigned char promedioB(unsigned char * img, unsigned length, unsigned BMPsize);
 
 
 
